a9c15ad2a7e5530464cb988f675ec962e46bc3cc
JarekD4 <yard4@outlook.com>
2025-01-15 20:56:44 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+#st.title("Ankieta Powitalna")
+
+
+
+st.header('Eksploaracja danych z Ankiety Powitalnej')
+
+if st.button('Cześć, zaczynamy?'):
+     st.write('Miło, że jesteś')
+else:
+    st.write('Do widzenia!')
+
+
+
+
+
